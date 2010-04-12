f85d07723313484e23fd9421aa47a2d47863c644
Paul A. Anokhin <paul7@paul7-dell.(none)>
2010-04-12 14:39:20 +0400

    Revert "work on compiling in progress"
    This reverts commit a352ac84c95b39266469ff89042916aa6a0eef84.
    
    Conflicts:
    
    	refal-compiler.lisp

--- refal-compiler.lisp
+++ refal-compiler.lisp
@@ -14,47 +14,8 @@
 
 ;;; test functions
 
-(defclass refal-module ()
-  ((function-dict
-    :initform (make-hash-table :test #'equalp)
-    :accessor function-dict)
-   (module-name
-    :initform 'main
-    :initarg :module-name
-    :accessor module-name)))
-
-(defun reset-module (module)
-  (with-accessors ((dict function-dict)) module
-    (setf dict (make-hash-table :test #'equalp))))
-
-(defun compile-function (module fname statements)
-  (with-accessors ((dict function-dict) 
-		   (name module-name)) module
-    (if (gethash fname dict)
-	(error (format nil "duplicate function ~a in module ~a" fname module))
-    (setf (gethash fname dict) (compile-multiple statements)))))
-
-(defun refal-call (module fname data)
-  (with-accessors ((dict function-dict) 
-		   (name module-name)) module
-    (let ((func (gethash fname dict)))
-      (if func
-	  (funcall func data)
-	  (error (format nil "no function ~a in module ~a" fname module))))))
-
 ;; compile simple statement
-(defun compile-multiple (statements)
-  (let ((compiled-statements 
-	 (mapcar #'(lambda (statement)
-		     (apply #'compile-statement statement))
-		 statements)))
-    (labels ((compiled (code data)
-	       (if code
-		   (or (funcall (first code) data)
-		       (compiled (rest code) data)))))
-      #'(lambda (data)
-	  (compiled compiled-statements data)))))
-	       
+
 (defun compile-statement (left right)
   (multiple-value-bind (pattern dict) (string->pattern left)
     (let ((construct (string->pattern right dict)))
